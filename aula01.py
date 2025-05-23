ff7353752522c7c4aaef38e8c473395ac869d9ef
pedroh-0701 <pedrohenriquedeosilva@gmail.com>
2025-05-23 16:52:00 -0300

    Update and rename aula01.py to aula01

--- aula01.py
+++
@@ -1,5 +0,0 @@
-idade = int(input('Digite sua idade:'))
-nome = str(input('Digite seu nome:'))
-ano = int(input('Digite o ano que você está:'))
-
-print('Seu nome é', nome, ',você tem', idade, 'e está no', ano, 'ano da escola.')
