d46bee6a453c62fede12eb8132ded0e755b6e753
pedroh-0701 <pedrohenriquedeosilva@gmail.com>
2025-05-23 16:48:08 -0300

    Create aula01.py

---
+++ aula01.py
@@ -0,0 +1,5 @@
+idade = int(input('Digite sua idade:'))
+nome = str(input('Digite seu nome:'))
+ano = int(input('Digite o ano que você está:'))
+
+print('Seu nome é', nome, ',você tem', idade, 'e está no', ano, 'ano da escola.')
