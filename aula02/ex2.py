053a80a7f3d0b96e839402ce8b6b93c9dca757ad
pedroh-0701 <pedrohenriquedeosilva@gmail.com>
2025-05-30 14:08:16 -0300

    Create ex2.py

---
+++ aula02/ex2.py
@@ -0,0 +1,7 @@
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Você é maior de idade, você pode dirigir!")
+else:
+    print("Você é menor de idade, você não pode dirigir!")
+    
