4cfee4f01b2ce8b8c5e5fb38089646c18533845c
pedroh-0701 <pedrohenriquedeosilva@gmail.com>
2025-06-13 16:41:44 -0300

    Create ex2_geometria.py

---
+++ aula04/ex2_geometria.py
@@ -0,0 +1,11 @@
+def calcular_area_retangulo(base, altura):
+    area1 = base * altura
+    return area1
+
+def calcular_area_triangulo(base, altura):
+    area2 = (base * altura)/2
+    return area2
+
+def calcular_area_circulo(raio):
+    area3 = 3.14*raio
+    return area3
